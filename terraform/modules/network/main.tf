b48f159ade1a96e963597c2a41d56db1f6767d72
srija <srija@example.com>
2026-09-02 14:09:31 +1000

    corrected as per validate

--- terraform/modules/network/main.tf
+++ terraform/modules/network/main.tf
@@ -1,8 +1,8 @@
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.project_name}-${var.environment}-vnet"
   address_space       = ["10.0.0.0/16"]
-  location            = var.resource_group_name
-  resource_group_name = var.location
+  location            = var.location
+  resource_group_name = var.resource_group_name
 }
 
 resource "azurerm_subnet" "subnet" {
